8caf1cfcc396977b405f5b645e8b99f3ead100b9
Arihin <arihin.slava@yandex.ru>
2022-11-08 21:08:32 +0300

    Выполнил 1 задачу "Массивы"

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        /// Задача 1
+        System.out.println(" Задача 1");
+        int [] massive = new int[3];
+        double [] massive1 ={1.57, 7.654, 9.986};
+        int [] a = new  int[17];
     }
 }
