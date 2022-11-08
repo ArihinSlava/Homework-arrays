d531c6249f95f21a578578a2d2b49f57f5c7a239
Arihin <arihin.slava@yandex.ru>
2022-11-08 22:03:59 +0300

    Выполнил 2 задачу "Массивы"

--- Main.java
+++ Main.java
@@ -1,9 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        /// Задача 1
-        System.out.println(" Задача 1");
+
         int [] massive = new int[3];
+        massive[0] = 1;
+        massive[1] = 2;
+        massive[2] = 3;
+        for (int i = 0; i < massive.length; i++) {
+            if (i != 2) {
+                System.out.print(massive[i] + ", ");
+            } else
+                System.out.println(massive[i]);
+        }
+
         double [] massive1 ={1.57, 7.654, 9.986};
-        int [] a = new  int[17];
+        for (int i = 0; i < massive1.length; i++) {
+            if (i != 2) {
+                System.out.print(massive1[i] + ",");
+            } else
+                System.out.println(massive1[i]);
+        }
+        int [] number = new int[5];
+        number[0] = 50;
+        number[1] = 60;
+        number[2] = 70;
+        number[3] = 80;
+        number[4] = 90;
+        for (int i = 0; i < number.length; i++) {
+            if (i != 4) {
+                System.out.print(number[i] + ",");
+            } else
+                System.out.println(number[i]);
+        }
+
     }
 }
